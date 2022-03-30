dbc9ab5c78e68effff69a39f0c1d78457b717cc3
Joshua Guzman <Joshua_guzman615@yahoo.com>
2022-03-30 05:34:32 -0400

    Added Brick functionality

--- BrickbreakerGame/src/BrickGame/GamePlay.java
+++ BrickbreakerGame/src/BrickGame/GamePlay.java
@@ -71,8 +71,35 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
             //Checking if ball intersects with paddel
             if(new Rectangle(ballposX,ballposY,20,20).intersects(new Rectangle(playerX,550,100,8))){
                 ballYdir = -ballYdir;
-
             }
+            for(int i = 0; i < map.map.length; i++){
+                for(int j = 0; j< map.map.length; i++){
+                    if (map.map[i][j]>0){
+                        int brickX = j*map.brickWidth + 80;
+                        int brickY = i*map.brickHeight + 50;
+                        int brickWidth = map.brickWidth;
+                        int brickHeight = map.brickHeight;
+
+                        Rectangle rect = new Rectangle(brickX, brickY, brickWidth, brickHeight);
+                        Rectangle ballRect = new Rectangle(ballposX, ballposY, 20, 20);
+                        Rectangle brickRect = rect;
+
+                        if (ballRect.intersects(brickRect)){
+                            map.setBrickValue(0,i,j);
+                            totalBricks --;
+                            score +=5;
+
+                            if(ballposX + 19 <= brickRect.x || ballposX + 1 >= brickRect.x + brickRect.width){
+                                ballXdir = -ballXdir;
+                            }else {
+                                ballYdir = - ballYdir;
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
 
 
             ballposX += ballXdir;
--- BrickbreakerGame/src/BrickGame/MapGenerator.java
+++ BrickbreakerGame/src/BrickGame/MapGenerator.java
@@ -6,7 +6,7 @@ public class MapGenerator {
     public int map[][];
     public int brickWidth;
     public int brickHeight;
-
+    //Creates the space for the bricks
     public MapGenerator(int row, int col){
         map = new int [row][col];
         for(int i = 0; i< map.length; i++){
@@ -18,16 +18,26 @@ public class MapGenerator {
         brickHeight = 150/row;
 
     }
-
+    //draws the bricks on the screen
     public  void draw(Graphics2D g){
         for(int i = 0; i < map.length; i++){
             for(int j = 0; j< map[0].length; j++){
                 if(map[i][j] > 0){
                     g.setColor(Color.black);
                     g.fillRect(j*brickWidth + 80, i*brickHeight + 50, brickWidth, brickHeight);
-                
+                    //sets the white lines on brick
+                    g.setStroke(new BasicStroke(3));
+                    g.setColor( Color.white);
+                    g.drawRect(j*brickWidth + 80, i*brickHeight + 50, brickWidth, brickHeight);
                 }
             }
         }
     }
+    public void setBrickValue(int value, int row, int col){
+        map[row][col] = value;
+    }
+
+
+
+
 }
